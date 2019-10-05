9864a523ccffa986cdf0bc2b01b9b9de70f9010a
sneha <S533984@nwmissouri.edu>
2019-10-04 21:59:57 -0500

    Add back button action for login

--- app/src/main/java/com/example/project/AdminLoginActivity.java
+++ app/src/main/java/com/example/project/AdminLoginActivity.java
@@ -2,7 +2,9 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminLoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class AdminLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+    }
+    public void onAdminBack(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivityForResult(intent,1);
     }
 }
