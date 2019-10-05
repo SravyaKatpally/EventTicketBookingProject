2292267ed1872d4142c9d8bbfe16917954a03768
Sai Sri Dachepalli <s534883@nwmissouri.edu>
2019-10-04 21:56:30 -0500

    Add back button action for signup page

--- app/src/main/java/com/example/project/SignUpActivity.java
+++ app/src/main/java/com/example/project/SignUpActivity.java
@@ -2,7 +2,9 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void onBackClick(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivityForResult(intent,1);
     }
 }
