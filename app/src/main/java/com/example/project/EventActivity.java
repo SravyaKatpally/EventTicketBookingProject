af55046c377f256dfff14a8a3071a40462c9b9e6
katpallysravya@gmail.com <password@99>
2019-10-26 23:39:22 -0500

    changes in event list

--- app/src/main/java/com/example/project/EventActivity.java
+++ app/src/main/java/com/example/project/EventActivity.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -43,12 +44,20 @@ public class EventActivity extends AppCompatActivity {
                 Spinner s = findViewById(R.id.spinner);
                 TextView o = findViewById(R.id.bookedTV);
                 o.setText(s.getSelectedItem().toString());
-                TextView numTicket = findViewById(R.id.numTicketsTV);
-                Integer oo = Integer.parseInt(numTicket.getText().toString());
-                TextView numOfTickets = findViewById(R.id.bookedTV);
-                Integer c = Integer.parseInt(numOfTickets.getText().toString());
-                Integer a = oo - c;
-                numTicket.setText(a.toString());
+                if(Integer.parseInt(o.getText().toString()) ==0)
+                {
+                    Toast.makeText(getApplicationContext(),
+                            "Select num of tickets",Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    TextView numTicket = findViewById(R.id.numTicketsTV);
+                    Integer oo = Integer.parseInt(numTicket.getText().toString());
+                    TextView numOfTickets = findViewById(R.id.bookedTV);
+                    Integer c = Integer.parseInt(numOfTickets.getText().toString());
+                    Integer a = oo - c;
+                    numTicket.setText(a.toString());
+                }
             }
 
             @Override
@@ -60,9 +69,19 @@ public class EventActivity extends AppCompatActivity {
     }
 
     public void onConfirm(View view) {
-        Intent intent = new Intent(this, ConfirmActivity.class);
-        startActivityForResult(intent, 1);
-    }
+        TextView numOfTickets = findViewById(R.id.bookedTV);
+        if(Integer.parseInt(numOfTickets.getText().toString()) == 0)
+        {
+            Toast.makeText(getApplicationContext(),
+                    "Select num of tickets",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Intent intent = new Intent(this, ConfirmActivity.class);
+            startActivityForResult(intent, 1);
+        }
+        }
+
 
     public void onBack(View view) {
         Intent intent = new Intent(this, EventListActivity.class);
