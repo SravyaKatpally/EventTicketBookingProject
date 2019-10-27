a6f9ad005983f28f09f11911de736b57fc2e6032
sneha <S533984@nwmissouri.edu>
2019-10-26 23:15:05 -0500

    changed functionality add_del_event java

--- app/src/main/java/com/example/project/AddingDeletingActivity.java
+++ app/src/main/java/com/example/project/AddingDeletingActivity.java
@@ -1,17 +1,24 @@
 
 package com.example.project;
 
-        import androidx.appcompat.app.AppCompatActivity;
 
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.TextView;
 
-        import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class AddingDeletingActivity extends AdminLoginActivity {
+
+    public static int NAME1 =0;
+    public static int DATE1 =1;
+    public static int TIME1 =2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +26,9 @@ public class AddingDeletingActivity extends AdminLoginActivity {
         setContentView(R.layout.activity_adding_deleting_events);
     }
 
-    public void onAddEvent(View view)
-    {
-        Intent intent = new Intent(this, AddEventActivity.class);
-        startActivityForResult(intent,1);
 
 
-    }
-
-    public void onDelete(View view)
-    {
+    public void onDelete(View view) {
         // Information of first delete
         TextView t1 = findViewById(R.id.textView15);
         TextView t2 = findViewById(R.id.textView2);
@@ -43,15 +43,13 @@ public class AddingDeletingActivity extends AdminLoginActivity {
         Button b1 = findViewById(R.id.button4);
         Button b2 = findViewById(R.id.button5);
 
-        if(b1 == view )
-        {
+        if (b1 == view) {
             t2.setVisibility(view.INVISIBLE);
             t3.setVisibility(view.INVISIBLE);
             t4.setVisibility(view.INVISIBLE);
             b1.setVisibility(view.INVISIBLE);
         }
-        if(b2 == view )
-        {
+        if (b2 == view) {
             t5.setVisibility(view.INVISIBLE);
             t6.setVisibility(view.INVISIBLE);
             t7.setVisibility(view.INVISIBLE);
@@ -59,6 +57,31 @@ public class AddingDeletingActivity extends AdminLoginActivity {
         }
     }
 
+    public void onaddEvent(View v)
+    {
+        Intent intent = new Intent(this, AddEventActivity.class);
+        startActivityForResult(intent, 1);
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode ==NAME1) {
+            String v = data.getStringExtra("NAME1");
+            String[] a = v.split(" ");
+
+
+            TextView t = findViewById(R.id.nameTV);
+            t.setText(a[0]);
+
+            TextView t1 = findViewById(R.id.dateTV);
+            t1.setText(a[1]);
+
+            TextView t2 = findViewById(R.id.timeTV);
+            t2.setText(a[2]);
+        }
+
+
+    }
    /* ArrayList<String> myList = (ArrayList<String>) getIntent().getSerializableExtra("mylist");
 
     public void onActivityResult(int requestCode, int resultCode, Intent data)
