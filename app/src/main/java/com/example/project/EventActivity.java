519f5609c267f055f0befdaeca82be6330c1a2a3
katpallysravya@gmail.com <password@99>
2019-10-26 20:23:24 -0500

    Changes in ticket booking. Booking max 5 tickets

--- app/src/main/java/com/example/project/EventActivity.java
+++ app/src/main/java/com/example/project/EventActivity.java
@@ -1,31 +1,34 @@
 package com.example.project;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.AdapterView;
-        import android.widget.ArrayAdapter;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.Spinner;
-        import android.widget.TextView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-        import org.w3c.dom.Text;
+import org.w3c.dom.Text;
 
-        import java.util.ArrayList;
-        import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class EventActivity extends EventListActivity {
-    //String h ="";
+public class EventActivity extends AppCompatActivity {
+    String h = "";
+    public static int EVENTNAME = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_activity);
 
         String[] arraySpinner = new String[]{
-                "1", "2", "3", "4", "5"
+                "0", "1", "2", "3", "4", "5"
         };
         Spinner s = findViewById(R.id.spinner);
 
@@ -34,26 +37,49 @@ public class EventActivity extends EventListActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s.setAdapter(adapter);
 
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Spinner s = findViewById(R.id.spinner);
+                TextView o = findViewById(R.id.bookedTV);
+                o.setText(s.getSelectedItem().toString());
+                TextView numTicket = findViewById(R.id.numTicketsTV);
+                Integer oo = Integer.parseInt(numTicket.getText().toString());
+                TextView numOfTickets = findViewById(R.id.bookedTV);
+                Integer c = Integer.parseInt(numOfTickets.getText().toString());
+                Integer a = oo - c;
+                numTicket.setText(a.toString());
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
 
-
-    public void onConfirm(View view)
-    {
+    public void onConfirm(View view) {
         Intent intent = new Intent(this, ConfirmActivity.class);
-        startActivityForResult(intent,1);
-
-
-
-
+        startActivityForResult(intent, 1);
     }
 
-    public void onBack(View view)
-    {
+    public void onBack(View view) {
         Intent intent = new Intent(this, EventListActivity.class);
-        startActivityForResult(intent,1);
+        startActivityForResult(intent, 1);
     }
 
 
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(resultCode == EVENTNAME)
+        {
+            String intentData = data.getStringExtra("EVENTNAME");
+            TextView tv = findViewById(R.id.displayTV);
+            tv.setText("lslhseilglfck "+intentData);
+        }
+    }
 }
