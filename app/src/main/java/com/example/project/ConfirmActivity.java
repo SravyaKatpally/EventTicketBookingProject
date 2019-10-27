0add5f4b15f81196d306de9046ea975ebabefccd
katpallysravya@gmail.com <password@99>
2019-10-26 23:33:18 -0500

    changes in confirmation page

--- app/src/main/java/com/example/project/ConfirmActivity.java
+++ app/src/main/java/com/example/project/ConfirmActivity.java
@@ -33,8 +33,8 @@ public class ConfirmActivity extends EventActivity {
 
     public void emailButton(View view)
     {
-        Button b = findViewById(R.id.button);
-        TextView tv = findViewById(R.id.displayTV);
+        Button b = findViewById(R.id.bookBTN);
+        TextView tv = findViewById(R.id.emailTV);
         if(view == b)
         {
             tv.setText("Mail is sent");
