f3c2b7a7f309251ad552ba936f27955df91e2e6f
vinusha09 <sandadivinushareddy1994@gmail.com>
2019-11-16 20:23:17 -0600

    adding deleting

--- app/src/main/java/com/example/project/AddingDeletingActivity.java
+++ app/src/main/java/com/example/project/AddingDeletingActivity.java
@@ -3,27 +3,49 @@ package com.example.project;
 
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class AddingDeletingActivity extends AdminLoginActivity {
 
     public static int NAME1 =0;
+    public static int LOGIN =0;
     public static int DATE1 =1;
     public static int TIME1 =2;
+
+    private String name1 = "";
+    private String date1 = "";
+    private String time1 = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adding_deleting_events);
+
+        Button b1 = findViewById(R.id.thirdBook);
+        Button b2 = findViewById(R.id.thirdDelete);
+
+        b1.setVisibility(View.INVISIBLE);
+        b2.setVisibility(View.INVISIBLE);
+
+        Button bb1 = findViewById(R.id.fd2);
+        Button bb2 = findViewById(R.id.delete2);
+        Button bb3 = findViewById(R.id.thirdDelete);
+
+        bb1.setVisibility(View.INVISIBLE);
+        bb2.setVisibility(View.INVISIBLE);
+        bb3.setVisibility(View.INVISIBLE);
+
     }
 
-
-
-    public void onDelete(View view) {
+    public void onDelete(View view)
+    {
         // Information of first delete
         TextView t1 = findViewById(R.id.e1);
         TextView t2 = findViewById(R.id.d1);
@@ -37,7 +59,6 @@ public class AddingDeletingActivity extends AdminLoginActivity {
         TextView t22 = findViewById(R.id.time2);
         Button b3 = findViewById(R.id.book2);
         Button b4 = findViewById(R.id.delete2);
-
 
         TextView t30 = findViewById(R.id.thirdEvent);
         TextView t31 = findViewById(R.id.thirdDate);
@@ -72,7 +93,10 @@ public class AddingDeletingActivity extends AdminLoginActivity {
     {
         Intent intent = new Intent(this, AddEventActivity.class);
         startActivityForResult(intent, 1);
-
+        Button b1 = findViewById(R.id.thirdBook);
+        Button b2 = findViewById(R.id.thirdDelete);
+        b1.setVisibility(View.VISIBLE);
+        b2.setVisibility(View.VISIBLE);
     }
 
     public void onBookFootball(View v)
@@ -87,27 +111,32 @@ public class AddingDeletingActivity extends AdminLoginActivity {
         startActivityForResult(intent, 1);
     }
 
-
     public void onBackClick(View v)
     {
         Intent intent = new Intent(this, AdminLoginActivity.class);
         startActivityForResult(intent, 1);
-
     }
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(resultCode ==NAME1) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(resultCode ==NAME1)
+        {
+
+            Button bb1 = findViewById(R.id.fd2);
+            Button bb2 = findViewById(R.id.delete2);
+            Button bb3 = findViewById(R.id.thirdDelete);
+
             String v = data.getStringExtra("NAME1");
             String[] a = v.split(" ");
-
-
             TextView t = findViewById(R.id.thirdEvent);
             t.setText(a[0]);
-
             TextView t1 = findViewById(R.id.thirdDate);
             t1.setText(a[1]);
-
             TextView t2 = findViewById(R.id.thirdTime);
             t2.setText(a[2]);
+
+            bb1.setVisibility(View.INVISIBLE);
+            bb2.setVisibility(View.INVISIBLE);
+            bb3.setVisibility(View.INVISIBLE);
         }
     }
 }
